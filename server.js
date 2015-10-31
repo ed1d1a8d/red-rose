a89183f1dbb379767f9195c317eb6e15fd8c4fb2
tonynater <tong.t.wang@gmail.com>
2015-10-31 16:31:54 -0700

    Changed server to https only

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
+var fs = require('fs'),
+    https = require('https'),
+    express = require('express'),
+    app = express();
 
 var logger = require('morgan');
 app.use(logger('dev'));
@@ -13,7 +15,8 @@ app.use('/', express.static('static'));
 app.use('/track', require('./routes/track/track'));
 
 //create server
-var http = require('http');
-
-http.createServer(app).listen(2222);
-console.log("Listening on port 2222");
+https.createServer({
+  key: fs.readFileSync('.ssl/p256v1-private-test.key'),
+  cert: fs.readFileSync('.ssl/red-rose-test.crt'),
+}, app).listen(2222);
+console.log("https server listening on port 2222");
