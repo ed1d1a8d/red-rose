e3450bb15d57b441aa0e36957f4e2d87d06efe1f
tonynater <tong.t.wang@gmail.com>
2015-11-01 02:31:38 -0800

    Added custom error messages

--- server.js
+++ server.js
@@ -9,10 +9,39 @@ app.use(logger('dev'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', express.static('static'));
+//static content
+app.use('/', express.static('static/public/html'));
+app.use('/', express.static('static/public/css'));
+app.use('/', express.static('static/public/images'));
+app.use('/humans.txt', express.static('static/public/humans.txt'));
+app.use('/robots.txt', express.static('static/public/robots.txt'));
 
 //routes
 app.use('/track', require('./routes/track/track'));
+
+//404 - File or directory not found
+app.use(function(req, res, next) {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile('static/private/404.html', {root: __dirname});
+  } else if (req.accepts('json')) {
+    res.send({ error: 'File or directory not found' });
+  }else {
+    res.type('txt').send('File or directory not found :(');
+  }
+});
+
+//500 - Internal Server Error
+app.use(function(error, req, res, next) {
+  res.status(500);
+  if (req.accepts('html')) {
+    res.sendfile('static/private/500.html', {root: __dirname});
+  } else if (req.accepts('json')) {
+    res.send({ error: 'Internal server error.' });
+  } else {
+    res.type('txt').send('Internal server error.');
+  }
+});
 
 //create server
 https.createServer({
